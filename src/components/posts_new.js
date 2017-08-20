b03b920e81ef33bf9c1355e54234055e2a191f37
AlexMollohan <ajmollohan@gmail.com>
2017-08-20 19:11:26 -0400

    Added field inputs with redux form, created fields and generalized their functionality

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
-  render() {
-    return (
-      <div>
-        new posts
+
+  // This is the component method which will be going into the render method
+  renderField(field) { // The field argument tells this function what it is targeting
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+          />
       </div>
+    )
+    // field.input is an object which contains event handlers and props, the '...' allows all of
+    // these props to be communicated to the input tag. Most pre-generated event handlers come through this.
+  }
+
+
+  render() {
+    // The component property of field is what will actually be used to render
+      // the JSX as is needed, you need to pass it a function and that function will
+      // handle the rendering
+    return (
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField} />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+          />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+          />
+      </form>
     )
   }
 }
 
-export default PostsNew
+export default reduxForm({
+  // The string assigned to this form property needs to be unique
+  form: 'PostsNewForm'
+})(PostsNew)
+
+// When connect helper is used, we use map state to props and map dispatch to props
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
+// now bringing in Redux form, a library that we could likely implement ourselves,
+  // but we will use the package instead in order to save time
+import { reducer as formReducer } from 'redux-form'
 import PostsReducer from './reducer_posts'
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  // setting it up with this keyword allows you to link all forms to the formReducer imported
+  form: formReducer
 });
 
 export default rootReducer;
