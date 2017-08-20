89bdbd12168ed385413e1898db2bdae8fc8ff8d2
AlexMollohan <ajmollohan@gmail.com>
2017-08-20 19:35:43 -0400

    Added form validation and set up forms to take in values for submission

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -13,26 +13,37 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
           />
+        {field.meta.error}
       </div>
     )
     // field.input is an object which contains event handlers and props, the '...' allows all of
     // these props to be communicated to the input tag. Most pre-generated event handlers come through this.
+
+    // Validate function automatically adds the meta.error property to the field, this is how front end error handling is done
+  }
+
+  onSubmit(values) {
+    console.log(values)
   }
 
 
   render() {
+    // The handle submit is a property being passed to the props by redux form
+    const { handleSubmit } = this.props
     // The component property of field is what will actually be used to render
       // the JSX as is needed, you need to pass it a function and that function will
       // handle the rendering
+
+    // handleSubmit is taking a method that we define and running that if redux-form clears it for submittal
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
           component={this.renderField} />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
           />
         <Field
@@ -40,12 +51,36 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
           />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     )
   }
 }
 
+function validate(values) {
+  // console.log(values) -> {titles:'test', categories:'test', content:'test'}
+  const errors = {}
+
+  // as can be presumed, validation is largely handled by if statements
+  if(!values.title){
+    errors.title = 'enter a title'
+  }
+  if(!values.categories){
+    errors.categories = 'enter a category'
+  }
+  if(!values.content){
+    errors.content = 'enter a body'
+  }
+
+  // validate the input from values
+
+  // If errors is empty, the form is fine to submit
+  // if errors has any properties whatsoever, redux will assume the form is invalid
+  return errors
+}
+
 export default reduxForm({
+  validate, // The addition of this property causes the redux form function to be fired automatically
   // The string assigned to this form property needs to be unique
   form: 'PostsNewForm'
 })(PostsNew)
